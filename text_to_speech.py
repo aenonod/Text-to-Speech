1bb829b79a4819108fad9ad75cf04d8bba7b2257
aenonod <abegail.nonod13@gmail.com>
2025-02-27 23:58:47 +0800

    initial commit

---
+++ text_to_speech.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+from playsound import playsound
+import tempfile
+import os
+
+text = input("Input your text: ")
+language = input("Enter your language (en for english, tl for tagalog): ")
+
+if language not in ['en', 'tl']:
+    print("Invalid language. Defaulting to english.")
+    language = 'en'
+
+output = gTTS(text=text, lang=language, slow=False)
+output.save("output.mp3")
+os.system("start output.mp3")
